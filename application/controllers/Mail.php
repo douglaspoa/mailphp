3182486cfb3551bdc1d15b408ad6d808c800449c
douglaspoa <dodosobreira@gmail.com>
2019-10-03 15:42:08 -0300

    começando projeto

---
+++ application/controllers/Mail.php
@@ -0,0 +1,33 @@
+<?php
+
+class Mail extends CI_Controller
+{
+	/**
+	 * Carrega o formulário
+	 * @param nenhum
+	 * @return view
+	 */
+	function index()
+	{
+		/*carrega a nossa view */
+		$this->load->view('mail');
+	}
+
+	function email()
+	{
+		$this->load->model('model_mail');
+
+		$data = [
+			'email' => $this->input->post('email', true),
+			'password' => $this->input->post('password', true)
+		];
+
+		$inbox = $this->model_mail->getEmailAttachments($data);
+
+		if ($inbox == false) {
+			$this->load->view('mail', 'Email ou senha incorretos');
+		}
+	}
+}
+
+?>
---
+++ application/helpers/functions_helper.php
@@ -0,0 +1,10 @@
+<?php
+
+function printrx($data){
+	echo "<pre>";
+	print_r($data);
+	echo "</pre>";
+	die();
+
+}
+?>
