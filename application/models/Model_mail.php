ca8be4c1e39a8b21dc004b952b895eeb5dcbde29
douglaspoa <dodosobreira@gmail.com>
2019-10-03 17:36:54 -0300

    utilizando lib imap

--- application/models/Model_mail.php
+++ application/models/Model_mail.php
@@ -8,121 +8,22 @@ class Model_mail extends CI_Model {
 	 * @param $data. Array que contém os dados para logar no email
 	 * @return boolean
 	 */
-	public function getEmailAttachments($data) {
-		$hostName = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
-		$userName = $data['email'];
-		$password = $data['password'];
+	public function getEmailAttachments($data)
+    {
+        include_once('lib/imap.php');
 
-		$inbox = imap_open($hostName,$userName,$password) or die(' Não foi possivel logar no email erro:' . imap_last_error());
+        $hostName = '{imap.gmail.com:993/imap/ssl/novalidate-cert}INBOX';
+        $userName = $data['email'];
+        $password = $data['password'];
 
-		$emails = imap_search($inbox,'ALL');
+        $email = new Imap();
+        $connect = $email->connect(
+            $hostName, //host
+            $userName, //username
+            $password //password
+        );
 
-		$max_emails = 1;
+        printrx($connect);
 
-		/* if any emails found, iterate through each email */
-		if($emails) {
-
-			$count = 1;
-
-			/* put the newest emails on top */
-			rsort($emails);
-
-			/* for every email... */
-			foreach($emails as $email_number)
-			{
-
-				/* get information specific to this email */
-				$overview = imap_fetch_overview($inbox,$email_number,0);
-
-				/* get mail message */
-				$message = imap_fetchbody($inbox,$email_number,2);
-
-				/* get mail structure */
-				$structure = imap_fetchstructure($inbox, $email_number);
-
-				$attachments = array();
-
-				/* if any attachments found... */
-				if(isset($structure->parts) && count($structure->parts))
-				{
-					for($i = 0; $i < count($structure->parts); $i++)
-					{
-						$attachments[$i] = array(
-							'is_attachment' => false,
-							'filename' => '',
-							'name' => '',
-							'attachment' => ''
-						);
-
-						if($structure->parts[$i]->ifdparameters)
-						{
-							foreach($structure->parts[$i]->dparameters as $object)
-							{
-								if(strtolower($object->attribute) == 'filename')
-								{
-									$attachments[$i]['is_attachment'] = true;
-									$attachments[$i]['filename'] = $object->value;
-								}
-							}
-						}
-
-						if($structure->parts[$i]->ifparameters)
-						{
-							foreach($structure->parts[$i]->parameters as $object)
-							{
-								if(strtolower($object->attribute) == 'name')
-								{
-									$attachments[$i]['is_attachment'] = true;
-									$attachments[$i]['name'] = $object->value;
-								}
-							}
-						}
-
-						if($attachments[$i]['is_attachment'])
-						{
-							$attachments[$i]['attachment'] = imap_fetchbody($inbox, $email_number, $i+1);
-
-							/* 4 = QUOTED-PRINTABLE encoding */
-							if($structure->parts[$i]->encoding == 3)
-							{
-								$attachments[$i]['attachment'] = base64_decode($attachments[$i]['attachment']);
-							}
-							/* 3 = BASE64 encoding */
-							elseif($structure->parts[$i]->encoding == 4)
-							{
-								$attachments[$i]['attachment'] = quoted_printable_decode($attachments[$i]['attachment']);
-							}
-						}
-					}
-				}
-
-				/* iterate through each attachment and save it */
-				foreach($attachments as $attachment)
-				{
-					if($attachment['is_attachment'] == 1)
-					{
-						$filename = $attachment['name'];
-						if(empty($filename)) $filename = $attachment['filename'];
-
-						if(empty($filename)) $filename = time() . ".dat";
-
-						/* prefix the email number to the filename in case two emails
-						 * have the attachment with the same file name.
-						 */
-						$fp = fopen("./" . $email_number . "-" . $filename, "w+");
-						fwrite($fp, $attachment['attachment']);
-						fclose($fp);
-					}
-
-				}
-
-				if($count++ >= $max_emails) break;
-			}
-
-		}
-
-		/* close the connection */
-		imap_close($inbox);
-
-	}
+    }
 }
